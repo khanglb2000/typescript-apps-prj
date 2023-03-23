1fc78fe73c52c10e03ca08a8785913fa9784dfdf
khanglb2000 <lambaokhang2000@gmail.com>
2023-03-23 15:05:29 +0700

    [RTS][ADD] Counter

--- vite-react-ts-project/src/App.tsx
+++ vite-react-ts-project/src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+import Counter from "./components/Counter";
 
 function App() {
+  const [ count, setCount ] = useState<number>(1)
+
   return (
-    <h1>Hello</h1>
+    <>
+      <Heading title={"Hello"} />
+      <Section title={"Subheading"}>
+        Welcome
+      </Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+    </>
   )
 }
 
