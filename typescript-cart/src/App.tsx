4765477734556669afaa87196f3eb355fe4df2f2
khanglb2000 <lambaokhang2000@gmail.com>
2023-03-27 08:55:05 +0700

    [ADD] Add header, footer

--- typescript-cart/src/App.tsx
+++ typescript-cart/src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react"
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ProductsList from "./components/ProductsList";
 
 function App() {
+  const [ viewCart, setViewCart ] = useState(false);
 
-  return (
-    <div></div>
+  const pageContent = viewCart ? <Cart/ > : <ProductsList />;
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      <Footer viewCart={viewCart} />
+    </>
   )
+
+  return content;
 }
 
 export default App
